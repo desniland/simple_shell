a29940f856346b021dc63092381f221264cad4a8
desniland <albertdesmond26@gmail.com>
2023-12-10 09:40:05 +0300

    error handling files

--- shell.h
+++ shell.h
@@ -27,11 +27,23 @@ int _myalias(info_t *info);
 
 /* _putchar */
 int _putchar(char c);
+void _eputs(char *str);
+int _eputchar(char c);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
 
 /* function3 */
 int _myexit(info_t *info);
 int _mycd(info_t *info);
 int _myhelp(info_t *info);
+
+/* function4 */
+int _myenv(info_t *info);
+char *_getenv(info_t *info, const char *name);
+int _mysetenv(info_t *info);
+int populate_env_list(info_t *info);
+
+/* function5 */
 
 
 #endif
